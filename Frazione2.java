0cfb523023b6698ec8747757bd0b695e0b1926ac
Riky091324872 <riccardo.cuoco07@gmail.com>
2024-11-08 08:10:19 +0000

    ciao

--- Frazione2.java
+++ Frazione2.java
@@ -7,23 +7,20 @@ public class Frazione2 {
         denominatore = 1;
     }
 
-    public void impostaFrazione(int num, int den) {
-        numeratore = num;
-        if (den != 0) {
-            denominatore = den;
-        } else {
-            System.out.println("Errore: il denominatore non può essere zero.");
-            denominatore = 1; // Default to 1 to avoid zero denominator
-        }
+    public Frazione2(int numeratore, int denominatore) {
+        this.numeratore = numeratore;
+        this.denominatore = (denominatore != 0) ? denominatore : 1;
     }
 
     public void semplifica() {
         int mcd = calcolaMCD(numeratore, denominatore);
-        numeratore = numeratore / mcd;
-        denominatore = denominatore / mcd;
+        numeratore /= mcd;
+        denominatore /= mcd;
     }
 
     private int calcolaMCD(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
         while (b != 0) {
             int temp = b;
             b = a % b;
@@ -32,12 +29,18 @@ public class Frazione2 {
         return a;
     }
 
-    public void stampaFrazione() {
-        System.out.println(numeratore + "/" + denominatore);
+    public String toString() {
+        return numeratore + "/" + denominatore;
     }
 
     public static void main(String[] args) {
-        Frazione2 f = new Frazione2(); // Changed to Frazione2
-        f.stampaFrazione();
+        Frazione2 frazione1 = new Frazione2();
+        System.out.println("Frazione iniziale: " + frazione1);
+
+        Frazione2 frazione2 = new Frazione2(8, 12);
+        System.out.println("Frazione non semplificata: " + frazione2);
+        
+        frazione2.semplifica();
+        System.out.println("Frazione semplificata: " + frazione2);
     }
 }
